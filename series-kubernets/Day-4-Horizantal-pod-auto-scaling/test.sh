8f17671bedf287d031759fa02c58afc26f7f370e
Anil Jadhav <aniljadhav@gmail.com>
2026-05-11 15:28:01 +0530

    Auto scaling added

---
+++ series-kubernets/Day-4-Horizantal-pod-auto-scaling/test.sh
@@ -0,0 +1,8 @@
+for j in {1..10}; do  
+    for i in {1..1000}; do  
+        curl -s -o /dev/null -w "%{http_code}\n" http://a3d36bd399d15400f8e03cbaaa3f46c9-163245138.us-east-1.elb.amazonaws.com/ &  
+    done  
+    wait  # Wait for all background curl processes to finish before next iteration
+done
+
+# TO increase the load we can use this sh file for testing purpose
